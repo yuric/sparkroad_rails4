a3b670b332e30e4d9ed8527fa6630bfb0b48e36c
Yuri Costa <yuri@sparkroad.com>
2013-04-09 01:58:16 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SparkroadR4::Application.config.secret_key_base = '6128dbb83c21fe05d4b641220fdfb5821057a6daa2f3c4b10c68c4e86469f19fe9d33b6678d836413a5017f5d08c826e78b51d9ffbdecf14932541149ce7d06c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SparkroadR4::Application.config.session_store :encrypted_cookie_store, key: '_sparkroad_r4_session'
