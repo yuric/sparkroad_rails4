5e5f5ec1cd8594b6818db4dc4793a34da81d1b53
Everton Moreth <everton.moreth@gmail.com>
2013-05-19 01:51:52 -0300

    First broadcast media tests

--- spec/models/broadcast_medium_spec.rb
+++ spec/models/broadcast_medium_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe BroadcastMedium do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "name" do
+    it 'should raise error if no name is given' do
+      expect {
+        BroadcastMedium.make! name: ''
+      }.to raise_error ActiveRecord::RecordInvalid
+    end
+
+    it 'should save if name is given' do
+      expect {
+        BroadcastMedium.make! name: 'Media 1'
+      }.to_not raise_error
+    end
+  end
+
+  describe "description" do
+    it 'should raise error if no description is given' do
+      expect {
+        BroadcastMedium.make! description: ''
+      }.to raise_error ActiveRecord::RecordInvalid
+    end
+
+    it 'should save if description is given' do
+      expect {
+        BroadcastMedium.make! description: 'Media Description 1'
+      }.to_not raise_error
+    end
+  end
 end
