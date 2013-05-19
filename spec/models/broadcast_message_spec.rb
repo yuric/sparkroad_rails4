4710f519cace2331f80192273b1fccd01ad150ba
Everton Moreth <everton.moreth@gmail.com>
2013-05-19 01:49:36 -0300

    First broadcast message tests

--- spec/models/broadcast_message_spec.rb
+++ spec/models/broadcast_message_spec.rb
@@ -1,5 +1,53 @@
 require 'spec_helper'
 
 describe BroadcastMessage do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:media_1) { BroadcastMedium.make! name: 'Media 1' }
+
+  describe "messages" do
+    it 'should raise error if both messages are empty' do
+      expect {
+        BroadcastMessage.make! english_message: '', spanish_message: ''
+      }.to raise_error ActiveRecord::RecordInvalid
+    end
+
+    it 'should save english only messages' do
+      expect {
+        BroadcastMessage.make! english_message: 'MESSAGE', spanish_message: ''
+      }.to_not
+    end
+
+    it 'should save spanish only messages' do
+      expect {
+        BroadcastMessage.make! english_message: '', spanish_message: 'MESSAGE'
+      }.to_not
+    end
+  end
+
+  describe "#media" do
+    it 'should raise error if no media or all_media is given' do
+      expect {
+        BroadcastMessage.make! all_media: false, media: nil
+      }.to raise_error ActiveRecord::RecordInvalid
+    end
+
+    it 'should raise error if all_media and another media is given' do
+      expect {
+        BroadcastMessage.make! all_media: true, media: media_1
+      }.to raise_error ActiveRecord::RecordInvalid
+    end
+
+    it 'should save if only all_media is given' do
+      expect {
+        BroadcastMessage.make! all_media: true, media: nil
+      }.to_not raise_error
+    end
+
+    it 'should save if only media is given' do
+      expect {
+        BroadcastMessage.make! all_media: false, media: media_1
+      }.to_not raise_error
+    end
+  end
+
 end
