b1609275d1ffed7eeee2fc196686ffa30536b2f0
Everton Moreth <everton.moreth@gmail.com>
2013-06-02 06:31:00 -0300

    Renamaming groups and group_items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130602063136) do
+ActiveRecord::Schema.define(version: 20130602072558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,12 +40,12 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.integer  "person_id"
   end
 
-  add_index "accounts", ["authentication_token"], name: "index_accounts_on_authentication_token", unique: true
-  add_index "accounts", ["confirmation_token"], name: "index_accounts_on_confirmation_token", unique: true
-  add_index "accounts", ["email"], name: "index_accounts_on_email", unique: true
-  add_index "accounts", ["person_id"], name: "index_accounts_on_person_id"
-  add_index "accounts", ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
-  add_index "accounts", ["unlock_token"], name: "index_accounts_on_unlock_token", unique: true
+  add_index "accounts", ["authentication_token"], name: "index_accounts_on_authentication_token", unique: true, using: :btree
+  add_index "accounts", ["confirmation_token"], name: "index_accounts_on_confirmation_token", unique: true, using: :btree
+  add_index "accounts", ["email"], name: "index_accounts_on_email", unique: true, using: :btree
+  add_index "accounts", ["person_id"], name: "index_accounts_on_person_id", using: :btree
+  add_index "accounts", ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true, using: :btree
+  add_index "accounts", ["unlock_token"], name: "index_accounts_on_unlock_token", unique: true, using: :btree
 
   create_table "broadcast_items", force: true do |t|
     t.integer  "broadcast_id"
@@ -55,9 +55,9 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
-  add_index "broadcast_items", ["broadcast_id"], name: "index_broadcast_items_on_broadcast_id"
-  add_index "broadcast_items", ["group_id"], name: "index_broadcast_items_on_group_id"
-  add_index "broadcast_items", ["student_id"], name: "index_broadcast_items_on_student_id"
+  add_index "broadcast_items", ["broadcast_id"], name: "index_broadcast_items_on_broadcast_id", using: :btree
+  add_index "broadcast_items", ["group_id"], name: "index_broadcast_items_on_group_id", using: :btree
+  add_index "broadcast_items", ["student_id"], name: "index_broadcast_items_on_student_id", using: :btree
 
   create_table "broadcast_media", force: true do |t|
     t.string   "name",        limit: 20, null: false
@@ -76,8 +76,8 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
-  add_index "broadcast_messages", ["broadcast_id"], name: "index_broadcast_messages_on_broadcast_id"
-  add_index "broadcast_messages", ["broadcast_media_id"], name: "index_broadcast_messages_on_broadcast_media_id"
+  add_index "broadcast_messages", ["broadcast_id"], name: "index_broadcast_messages_on_broadcast_id", using: :btree
+  add_index "broadcast_messages", ["broadcast_media_id"], name: "index_broadcast_messages_on_broadcast_media_id", using: :btree
 
   create_table "broadcasts", force: true do |t|
     t.string   "title"
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
-  add_index "broadcasts", ["school_id"], name: "index_broadcasts_on_school_id"
+  add_index "broadcasts", ["school_id"], name: "index_broadcasts_on_school_id", using: :btree
 
   create_table "parent_organizations", force: true do |t|
     t.string   "name"
@@ -100,6 +100,17 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
+  create_table "school_group_item_versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "school_group_item_versions", ["item_type", "item_id"], name: "index_school_group_item_versions_on_item_type_and_item_id", using: :btree
+
   create_table "school_group_items", force: true do |t|
     t.integer  "group_id"
     t.datetime "created_at"
@@ -108,9 +119,20 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.integer  "parent_id"
   end
 
-  add_index "school_group_items", ["group_id"], name: "index_school_group_items_on_group_id"
-  add_index "school_group_items", ["parent_id"], name: "index_school_group_items_on_parent_id"
-  add_index "school_group_items", ["student_id"], name: "index_school_group_items_on_student_id"
+  add_index "school_group_items", ["group_id"], name: "index_school_group_items_on_group_id", using: :btree
+  add_index "school_group_items", ["parent_id"], name: "index_school_group_items_on_parent_id", using: :btree
+  add_index "school_group_items", ["student_id"], name: "index_school_group_items_on_student_id", using: :btree
+
+  create_table "school_group_versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "school_group_versions", ["item_type", "item_id"], name: "index_school_group_versions_on_item_type_and_item_id", using: :btree
 
   create_table "school_groups", force: true do |t|
     t.string   "name"
@@ -119,7 +141,7 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
-  add_index "school_groups", ["school_id"], name: "index_school_groups_on_school_id"
+  add_index "school_groups", ["school_id"], name: "index_school_groups_on_school_id", using: :btree
 
   create_table "schools", force: true do |t|
     t.string   "name"
@@ -128,7 +150,7 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
-  add_index "schools", ["parent_organization_id"], name: "index_schools_on_parent_organization_id"
+  add_index "schools", ["parent_organization_id"], name: "index_schools_on_parent_organization_id", using: :btree
 
   create_table "students", force: true do |t|
     t.integer  "person_id"
@@ -136,7 +158,7 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
-  add_index "students", ["person_id"], name: "index_students_on_person_id"
+  add_index "students", ["person_id"], name: "index_students_on_person_id", using: :btree
 
   create_table "studies", force: true do |t|
     t.integer  "school_id"
@@ -145,8 +167,8 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
-  add_index "studies", ["school_id"], name: "index_studies_on_school_id"
-  add_index "studies", ["student_id"], name: "index_studies_on_student_id"
+  add_index "studies", ["school_id"], name: "index_studies_on_school_id", using: :btree
+  add_index "studies", ["student_id"], name: "index_studies_on_student_id", using: :btree
 
   create_table "teachers", force: true do |t|
     t.integer  "account_id"
@@ -154,7 +176,7 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
-  add_index "teachers", ["account_id"], name: "index_teachers_on_account_id"
+  add_index "teachers", ["account_id"], name: "index_teachers_on_account_id", using: :btree
 
   create_table "teaches", force: true do |t|
     t.integer  "group_id"
@@ -163,8 +185,8 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
-  add_index "teaches", ["group_id"], name: "index_teaches_on_group_id"
-  add_index "teaches", ["teacher_id"], name: "index_teaches_on_teacher_id"
+  add_index "teaches", ["group_id"], name: "index_teaches_on_group_id", using: :btree
+  add_index "teaches", ["teacher_id"], name: "index_teaches_on_teacher_id", using: :btree
 
   create_table "versions", force: true do |t|
     t.string   "item_type",  null: false
@@ -175,7 +197,7 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "created_at"
   end
 
-  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
   create_table "works", force: true do |t|
     t.integer  "teacher_id"
@@ -184,7 +206,7 @@ ActiveRecord::Schema.define(version: 20130602063136) do
     t.datetime "updated_at"
   end
 
-  add_index "works", ["school_id"], name: "index_works_on_school_id"
-  add_index "works", ["teacher_id"], name: "index_works_on_teacher_id"
+  add_index "works", ["school_id"], name: "index_works_on_school_id", using: :btree
+  add_index "works", ["teacher_id"], name: "index_works_on_teacher_id", using: :btree
 
 end
